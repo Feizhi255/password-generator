7d43333c1e488ee3477086457f9869ae953b1f94
Jordan Kirby <jrock255@hotmail.com>
2020-08-07 00:48:39 -0600

    added prompts to generator password btn

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,31 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// gen button
+var generateBtn = document.querySelector("#generate"); 
+//text area
+var passwordInput = document.getElementById('password'); 
+
+
 
 // Write password to the #password input
-function writePassword() {
+//will put final password onto the 'your secure password text area
+function writePassword(passwordInput) { 
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordReturn = document.getElementById('password').value;
+  var passwordText = document.querySelector("#password");  // text area
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ //generateBtn.addEventListener("click", writePassword,)
+
+ document.querySelector('#generate').addEventListener('click', function () {
+   var specialChar1 = confirm('Do you want this special character in your password?   "!"');
+   var specialChar2 = confirm('Do you want this special character in your password?   "@"');
+   var specialChar3 = confirm('Do you want this special character in your password?   "#"');
+   var specialChar4 = confirm('Do you want this special character in your password?   "$"');
+   console.log(specialChar1, specialChar2, specialChar3, specialChar3, specialChar4);
+ })
+
+
+
